4f1248468a027bcb291ae9aafe7c6ccde7da96e5
Tien01els <59406234+Tien01els@users.noreply.github.com>
2021-05-04 00:38:38 +0700

    Payroll
    Payroll

---
+++ FinalDBMS/FinalDBMS/C-SHARPCODE/Salary.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalDBMS
+{
+    public class Salary
+    {
+        Mydb db = new Mydb();
+        public bool AddCheckIn(string ID)
+        {
+            
+            SqlCommand command = new SqlCommand("INSERT INTO dbo.WORK (IDEmployee, CheckIn) VALUES (@IDEmployee, GETDATE())", db.getConnection);
+            command.Parameters.Add("@IDEmployee", System.Data.SqlDbType.NVarChar).Value = ID;
+            db.Openconnection();
+            if (command.ExecuteNonQuery() == 1)
+            {
+                db.Closeconnection();
+                return true;
+            }
+            else
+            {
+                db.Closeconnection();
+                return false;
+
+            }
+        }
+        public bool AddCheckOut(string ID)
+        {
+            SqlCommand command = new SqlCommand("UPDATE WORK SET CheckOut = GETDATE() WHERE IDEmployee = @IDEmployee", db.getConnection);
+            command.Parameters.Add("@IDEmployee", System.Data.SqlDbType.NVarChar).Value = ID;
+            db.Openconnection();
+            if (command.ExecuteNonQuery() == 1)
+            {
+                db.Closeconnection();
+                return true;
+            }
+            else
+            {
+                db.Closeconnection();
+                return false;
+
+            }
+        }
+        public bool CheckIDEmployee (string ID)
+        {
+            SqlCommand command = new SqlCommand("SELECT * FROM EMPLOYEE WHERE IDEmployee = @IDEmployee", db.getConnection);
+            command.Parameters.Add("@IDEmployee", System.Data.SqlDbType.NVarChar).Value = ID;
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+            return data.Rows.Count > 0;
+        }
+        public bool CheckIDWork(string ID)
+        {
+            SqlCommand command = new SqlCommand("SELECT * FROM WORK WHERE IDEmployee = @IDEmployee", db.getConnection);
+            command.Parameters.Add("@IDEmployee", System.Data.SqlDbType.NVarChar).Value = ID;
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+            return data.Rows.Count > 0;
+        }
+        public DataTable ShowTimeKeeping()
+        {
+            SqlCommand command = new SqlCommand("SELECT IDEmployee, CheckIn, CheckOut FROM WORK", db.getConnection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+            return data;
+        }
+        public DataTable ShowSalary()
+        {
+            SqlCommand command = new SqlCommand("SELECT * FROM SALARY", db.getConnection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+            return data;
+        }
+    }
+}
---
+++ FinalDBMS/FinalDBMS/Form/Salary/Timekeeping.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace FinalDBMS
+{
+    partial class Timekeeping
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewTimeKeeping = new System.Windows.Forms.DataGridView();
+            this.btnCheckIn = new System.Windows.Forms.Button();
+            this.lblID = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.btnCheckOut = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTimeKeeping)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewTimeKeeping
+            // 
+            this.dataGridViewTimeKeeping.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewTimeKeeping.Location = new System.Drawing.Point(13, 61);
+            this.dataGridViewTimeKeeping.Name = "dataGridViewTimeKeeping";
+            this.dataGridViewTimeKeeping.RowHeadersWidth = 62;
+            this.dataGridViewTimeKeeping.RowTemplate.Height = 28;
+            this.dataGridViewTimeKeeping.Size = new System.Drawing.Size(709, 374);
+            this.dataGridViewTimeKeeping.TabIndex = 0;
+            // 
+            // btnCheckIn
+            // 
+            this.btnCheckIn.Location = new System.Drawing.Point(285, 12);
+            this.btnCheckIn.Name = "btnCheckIn";
+            this.btnCheckIn.Size = new System.Drawing.Size(105, 43);
+            this.btnCheckIn.TabIndex = 1;
+            this.btnCheckIn.Text = "Check In";
+            this.btnCheckIn.UseVisualStyleBackColor = true;
+            this.btnCheckIn.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(9, 23);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(30, 20);
+            this.lblID.TabIndex = 2;
+            this.lblID.Text = "ID:";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(51, 20);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(228, 26);
+            this.txtID.TabIndex = 3;
+            // 
+            // btnCheckOut
+            // 
+            this.btnCheckOut.Location = new System.Drawing.Point(412, 12);
+            this.btnCheckOut.Name = "btnCheckOut";
+            this.btnCheckOut.Size = new System.Drawing.Size(105, 43);
+            this.btnCheckOut.TabIndex = 4;
+            this.btnCheckOut.Text = "Check Out";
+            this.btnCheckOut.UseVisualStyleBackColor = true;
+            this.btnCheckOut.Click += new System.EventHandler(this.btnCheckOut_Click);
+            // 
+            // Timekeeping
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 447);
+            this.Controls.Add(this.btnCheckOut);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.btnCheckIn);
+            this.Controls.Add(this.dataGridViewTimeKeeping);
+            this.Name = "Timekeeping";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Timekeeping";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTimeKeeping)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewTimeKeeping;
+        private System.Windows.Forms.Button btnCheckIn;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Button btnCheckOut;
+    }
+}
